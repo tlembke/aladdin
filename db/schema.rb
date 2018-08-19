3ce7466616fa06ce055a7993b6bc487c6a9aa188
Tony Lembke <tony@lemlink.com.au>
2018-08-19 14:38:26 +1000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20180624110837) do
     t.integer  "measure_id"
     t.integer  "active"
     t.integer  "parent"
-    t.integer  "master_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "master_id"
     t.decimal  "target"
     t.string   "autoload"
   end
